2aa5f0ba4a0ad01c4ac7994a505be732accdd431
Russell Heilling <russell@linx.net>
2014-06-15 15:11:38 +0100

    Bump version to 0.11.1

--- source.py
+++ source.py
@@ -22,7 +22,7 @@ class Comicvine(Source):
   name = 'Comicvine'
   description = 'Downloads metadata and covers from Comicvine'
   author = 'Russell Heilling'
-  version = (0, 10, 6)
+  version = (0, 11, 1)
   capabilities = frozenset(['identify', 'cover'])
   touched_fields = frozenset([
       'title', 'authors', 'comments', 'publisher', 'pubdate', 'series',
