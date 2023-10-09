4957e68a945970090325279ed215313048fbdea6
Bernardo Bandos <jbbandos@hotmail.com>
2023-10-09 08:24:43 +0200

    Now will remove extranous spaces from API key First linting and reformating, to continue

--- __init__.py
+++ __init__.py
@@ -1,33 +1,86 @@
-'''
+"""
 calibre_plugins.comicvine - A calibre metadata source for comicvine
-'''
+"""
 from calibre_plugins.comicvine.source import Comicvine
 
-if __name__ == '__main__':
-  from calibre import prints
-  from calibre.ebooks.metadata.sources.test import (test_identify_plugin,
-                                                    title_test, authors_test,
-                                                    series_test)
-  test_identify_plugin(Comicvine.name, [
-      (
-        {
-          'title': 'heavy metal magazine #202008: vol. 300: all star special',
-          'authors': ['Various', 'Claudia Scarletgothica', 'Dave Sharpe', 'Glenn Fabry']
-          },
+if __name__ == "__main__":
+    from calibre import prints
+    from calibre.ebooks.metadata.sources.test import (
+        test_identify_plugin,
+        title_test,
+        authors_test,
+        series_test,
+    )
+
+    test_identify_plugin(
+        Comicvine.name,
         [
-          title_test('heavy metal magazine #202008: vol. 300: all star special', 
-                     exact=False),
-          authors_test(['duke mighten', 'jackson butch guice', 'mœbius', 'paul fry', 
-          'agustin alessio', 'dave sharpe', 'jim terry', 'richard corben', 'protobunker studio', 
-          'bruce edwards', 'geoff boucher', 'frank forte', 'dylan sprouse', 'northworld', 
-          'bryan alvarez', 'brennan wagner', 'mark bode', 'giuseppe cafaro', 'joshua sky', 
-          'mark mccann', 'glenn fabry', 'matthew medney', 'vaughn bode', 'kent williams', 
-          'jaime martinez', 'justin mohlman', 'patrick norbert', 'adam brown', 'kelley jones', 
-          'al barrionuevo', 'brendan columbus', 'claudia scarletgothica', 'chris sotomayor', 
-          'marshall dillon', 'dan berger', 'german ponce', 'candice han', 'diego yapur', 
-          'justin jordan', 'albert patin de la fizeliere', 'r. g. llarena', 'stephanie phillips', 
-          'tater 7', 'david erwin', 'george c. romero', 'blake northcott']),
-         ]
-      ), 
-    ]
-)
+            (
+                {
+                    "title": "heavy metal magazine #202008: vol. 300: all star special",
+                    "authors": [
+                        "Various",
+                        "Claudia Scarletgothica",
+                        "Dave Sharpe",
+                        "Glenn Fabry",
+                    ],
+                },
+                [
+                    title_test(
+                        "heavy metal magazine #202008: vol. 300: all star special",
+                        exact=False,
+                    ),
+                    authors_test(
+                        [
+                            "duke mighten",
+                            "jackson butch guice",
+                            "mœbius",
+                            "paul fry",
+                            "agustin alessio",
+                            "dave sharpe",
+                            "jim terry",
+                            "richard corben",
+                            "protobunker studio",
+                            "bruce edwards",
+                            "geoff boucher",
+                            "frank forte",
+                            "dylan sprouse",
+                            "northworld",
+                            "bryan alvarez",
+                            "brennan wagner",
+                            "mark bode",
+                            "giuseppe cafaro",
+                            "joshua sky",
+                            "mark mccann",
+                            "glenn fabry",
+                            "matthew medney",
+                            "vaughn bode",
+                            "kent williams",
+                            "jaime martinez",
+                            "justin mohlman",
+                            "patrick norbert",
+                            "adam brown",
+                            "kelley jones",
+                            "al barrionuevo",
+                            "brendan columbus",
+                            "claudia scarletgothica",
+                            "chris sotomayor",
+                            "marshall dillon",
+                            "dan berger",
+                            "german ponce",
+                            "candice han",
+                            "diego yapur",
+                            "justin jordan",
+                            "albert patin de la fizeliere",
+                            "r. g. llarena",
+                            "stephanie phillips",
+                            "tater 7",
+                            "david erwin",
+                            "george c. romero",
+                            "blake northcott",
+                        ]
+                    ),
+                ],
+            ),
+        ],
+    )
--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import time
 from PyQt5.Qt import QWidget, QGridLayout, QLabel, QLineEdit
 from calibre.utils.config import JSONConfig
 
-from calibre_plugins.comicvine  import pycomicvine
+from calibre_plugins.comicvine import pycomicvine
 
 PREFS = JSONConfig('plugins/comicvine')
 PREFS.defaults['api_key'] = ''
@@ -18,47 +18,48 @@ PREFS.defaults['requests_update'] = time.time()
 PREFS.defaults['max_volumes'] = 2
 pycomicvine.api_key = PREFS['api_key']
 
+
 class ConfigWidget(QWidget):
-  'Configuration widget'
-  def __init__(self):
-    QWidget.__init__(self)
-    self.layout = QGridLayout()
-    self.layout.setSpacing(10)
-    self.setLayout(self.layout)
+    'Configuration widget'
 
-    self.key_label = QLabel('&api key:')
-    self.key_msg = QLineEdit(self)
-    self.key_msg.setText(PREFS['api_key'])
-    self.layout.addWidget(self.key_label, 1, 0)
-    self.layout.addWidget(self.key_msg, 1, 1, 1, 20)
-    self.key_label.setBuddy(self.key_msg)
+    def __init__(self):
+        QWidget.__init__(self)
+        self.layout = QGridLayout()
+        self.layout.setSpacing(10)
+        self.setLayout(self.layout)
 
-    self.threads_label = QLabel('&worker_threads:')
-    self.threads_msg = QLineEdit(self)
-    self.threads_msg.setText(str(PREFS['worker_threads']))
-    self.layout.addWidget(self.threads_label, 2, 0)
-    self.layout.addWidget(self.threads_msg, 2, 1)
-    self.threads_label.setBuddy(self.threads_msg)
+        self.key_label = QLabel('&api key:')
+        self.key_msg = QLineEdit(self)
+        self.key_msg.setText(PREFS['api_key'])
+        self.layout.addWidget(self.key_label, 1, 0)
+        self.layout.addWidget(self.key_msg, 1, 1, 1, 20)
+        self.key_label.setBuddy(self.key_msg)
 
-    self.rate_label = QLabel('API &Requests per second:')
-    self.rate_msg = QLineEdit(self)
-    self.rate_msg.setText(str(PREFS['requests_rate']))
-    self.layout.addWidget(self.rate_label, 3, 0)
-    self.layout.addWidget(self.rate_msg, 3, 1)
-    self.rate_label.setBuddy(self.rate_msg)
+        self.threads_label = QLabel('&worker_threads:')
+        self.threads_msg = QLineEdit(self)
+        self.threads_msg.setText(str(PREFS['worker_threads']))
+        self.layout.addWidget(self.threads_label, 2, 0)
+        self.layout.addWidget(self.threads_msg, 2, 1)
+        self.threads_label.setBuddy(self.threads_msg)
 
-    self.maxvol_label = QLabel('&Maximum # of volumes returned:')
-    self.maxvol_msg = QLineEdit(self)
-    self.maxvol_msg.setText(str(PREFS['max_volumes']))
-    self.layout.addWidget(self.maxvol_label, 4, 0)
-    self.layout.addWidget(self.maxvol_msg, 4, 1)
-    self.maxvol_label.setBuddy(self.maxvol_msg)
+        self.rate_label = QLabel('API &Requests per second:')
+        self.rate_msg = QLineEdit(self)
+        self.rate_msg.setText(str(PREFS['requests_rate']))
+        self.layout.addWidget(self.rate_label, 3, 0)
+        self.layout.addWidget(self.rate_msg, 3, 1)
+        self.rate_label.setBuddy(self.rate_msg)
 
-  def save_settings(self):
-    'Apply new settings value'
-    PREFS['api_key'] = str(self.key_msg.text())
-    PREFS['worker_threads'] = int(self.threads_msg.text())
-    PREFS['requests_rate'] = int(self.rate_msg.text())
-    PREFS['max_volumes'] = int(self.maxvol_msg.text())
-    pycomicvine.api_key = PREFS['api_key']
+        self.maxvol_label = QLabel('&Maximum # of volumes returned:')
+        self.maxvol_msg = QLineEdit(self)
+        self.maxvol_msg.setText(str(PREFS['max_volumes']))
+        self.layout.addWidget(self.maxvol_label, 4, 0)
+        self.layout.addWidget(self.maxvol_msg, 4, 1)
+        self.maxvol_label.setBuddy(self.maxvol_msg)
 
+    def save_settings(self):
+        'Apply new settings value'
+        PREFS['api_key'] = str(self.key_msg.text()).strip()
+        PREFS['worker_threads'] = int(self.threads_msg.text())
+        PREFS['requests_rate'] = int(self.rate_msg.text())
+        PREFS['max_volumes'] = int(self.maxvol_msg.text())
+        pycomicvine.api_key = PREFS['api_key']
